f75758d36e13e19de42eddfe06794a5a6e6d24cb
illusive-chase <ye_kai@pku.edu.cn>
2023-09-21 16:40:59 +0800

    init

---
+++ assets/zsh.sh
@@ -0,0 +1,3 @@
+sudo apt-get install zsh && \
+cp ./assets/oh-my-zsh $HOME/.oh-my-zsh -r && \
+cp ./assets/zshrc $HOME/.zshrc
--- zsh.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install zsh && \
-cp ./oh-my-zsh $HOME/.oh-my-zsh -r && \
-cp ./zshrc $HOME/.zshrc
