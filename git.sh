ee279eac34eae1bba859c645e27aea147d379c57
illusive-chase <ye_kai@pku.edu.cn>
2024-01-21 16:03:20 +0800

    update git ssh config

--- git.sh
+++ git.sh
@@ -4,4 +4,11 @@ ssh-keygen -t rsa -C "ye_kai@pku.edu.cn" && \
 cat $HOME/.ssh/id_rsa.pub && \
 echo "\nAny Press..." && \
 read -s -n 1 && \
+touch $HOME/.ssh/config && \
+echo "Host github.com" >> $HOME/.ssh/config && \
+echo "  Hostname ssh.github.com" >> $HOME/.ssh/config && \
+echo "  User git" >> $HOME/.ssh/config && \
+echo "  PreferredAuthentications publickey" >> $HOME/.ssh/config && \
+echo "  IdentityFile ~/.ssh/id_rsa" >> $HOME/.ssh/config && \
+echo "  Port 443" >> $HOME/.ssh/config && \
 ssh -T git@github.com
