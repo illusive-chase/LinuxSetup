81dbe2af3ea0a3bda8e20aeda9d3c07aa8625741
illusive-chase <ye_kai@pku.edu.cn>
2024-06-18 21:25:38 +0800

    update

--- adu.sh
+++ adu.sh
@@ -1,19 +1,21 @@
 # user configs
 
+ADMIN=user
+
 mkdir /data/$1 && \
-sudo useradd -g dpku $1 && \
+sudo useradd -g $ADMIN $1 && \
 echo ">>> Passwd >>>" && sudo passwd $1 && \
 sudo usermod -d /data/$1 -s /bin/zsh $1 && \
-cp /home/dpku/LinuxSetup/assets/oh-my-zsh /data/$1/.oh-my-zsh -r && \
-cp /home/dpku/LinuxSetup/assets/zshrc /data/$1/.zshrc && \
+cp /home/$ADMIN/LinuxSetup/assets/oh-my-zsh /data/$1/.oh-my-zsh -r && \
+cp /home/$ADMIN/LinuxSetup/assets/zshrc /data/$1/.zshrc && \
 sudo chown $1 -R /data/$1 && \
 
 echo "User configs are set." && \
 
 # miniconda
 
-cp /home/dpku/Miniconda3-latest-Linux-x86_64.sh /data/$1 && \
-echo "bash /data/$1/Miniconda3-latest-Linux-x86_64.sh && /data/$1/miniconda3/bin/conda init zsh && /data/$1/miniconda3/bin/conda config --set changeps1 False && source /data/$1/.zshrc" > /data/$1/install.sh && \
+cp /home/$ADMIN/Miniconda3-latest-Linux-x86_64.sh /data/$1 && \
+echo "bash /data/$1/Miniconda3-latest-Linux-x86_64.sh && /data/$1/miniconda3/bin/conda init zsh && /data/$1/miniconda3/bin/conda config --set changeps1 False && rm /data/$1/Miniconda3-latest-Linux-x86_64.sh /data/$1/install.sh -f" > /data/$1/install.sh && \
 
 echo "Miniconda installation script is set." && \
 
